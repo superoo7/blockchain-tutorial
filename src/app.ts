04ea3efd390b4e7f606c3d1fc2157d128e8b0b62
Lai Weng Han <jlwhoo7@gmail.com>
2018-04-04 12:56:48 +0800

    Added mining part

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import Blockchain from './blockchain';
 import Block from './block';
+import Transaction from './transaction';
 
-
+// create a genesis block
 let b = new Block();
+
+// initialize blockchain with genesis block
 let bc = new Blockchain(b);
 
+// create a transaction
+let t = new Transaction('me', 'you', 7);
 
-console.log(bc.blocks);
+// mining part
+let newB = bc.getNextBlock([t]);
+bc.addBlock(newB);
+
+console.log(bc.blocks[1].transactions);
